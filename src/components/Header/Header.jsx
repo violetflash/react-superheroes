5e100a8c09b250bb8e7fe3975cc5670abdd66796
Alex Grobovsky <ktotaam@mail.ru>
2021-08-12 09:24:26 +0500

    fix: disable nav menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './Header.module.scss';
 import Repo from "./Repo";
 import Logo from "./Logo/";
-import Nav from "./Nav";
+// import Nav from "./Nav";
 
 const Header = () => (
     <header className={s.Header}>
@@ -10,7 +10,7 @@ const Header = () => (
             <div className={s.Header__content}>
                 <div className={s.Header__leftSide}>
                     <Logo />
-                    <Nav />
+                    {/*<Nav />*/}
                 </div>
                 <Repo/>
             </div>
