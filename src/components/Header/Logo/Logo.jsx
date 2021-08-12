02eceb817a660098e6781d0a3ba251276eb4dba4
Alex Grobovsky <ktotaam@mail.ru>
2021-08-12 09:11:02 +0500

    fix: fixed logo importing

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import s from "./Logo.module.scss";
+import logo from './superhero.svg';
 
 const Logo = () => (
     <figure className={s.Logo}>
-        <img className={s.Logo__img} src="./superhero.svg" alt="Logo"/>
+        <img className={s.Logo__img} src={logo} alt="Logo"/>
         <figcaption className={s.Logo__text}>Super Heroes App</figcaption>
     </figure>
 );
